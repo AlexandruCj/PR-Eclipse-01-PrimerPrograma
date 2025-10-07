b91baaef39eaf6b9a1cb05e16a4235937255b651
AlexandruCj <pbazcoj2912@g.educaand.es>
2025-10-07 20:40:57 +0200

    Add files via upload

---
+++ src/tema2_eclipse/HelloEclipse.java
@@ -0,0 +1,29 @@
+package tema2_eclipse;
+
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+		/*
+		 * Autor: Pedro Alexandro Bazan Cojan Fecha: 07/10/2025
+		 */
+		
+
+		System.out.println("¡Mi primer programa!");// Este programa imprime mensajes en consola
+		
+		int contadorMensajes = 0;
+		contadorMensajes++;
+		System.out.println("Mensajes impresos: " + contadorMensajes);
+		
+		System.out.println("Reglas básicas de Java:");// Este programa imprime mensajes en consola
+		System.out.println("1) Distingue MAYÚSCULAS y minúsculas");// Este programa imprime mensajes en consola
+		System.out.println("2) Cada instrucción termina con ;");// Este programa imprime mensajes en consola
+		
+		
+		int x = 3;
+		System.out.print("Este código no está");
+		System.out.print(" muy bien ");
+		System.out.print("tabulado");
+		x = 7;
+		System.out.println();
+	}
+}
